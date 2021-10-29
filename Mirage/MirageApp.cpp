053131cef70260fa15b17a9520d6dcc991deb0f1
Elvis Cirikovic <ccirikovic.elvis@gmail.com>
2021-10-29 13:35:18 -0400

    i got so many errors here lol

--- Mirage/MirageApp.cpp
+++ Mirage/MirageApp.cpp
@@ -1,16 +1,23 @@
 #include "pch.h"
 #include "MirageApp.h"
-
+#include  "GLFW/glfw3.h"
 
 namespace Mirage
 {
 	void MirageApp::Run()
 	{
 		std::cout << "Mirage is running..." << std::endl;
+		GLFWwindow* window;
+
+		glfwInit();
+
+		window = glfwCreateWindow(800, 600, "test", NULL, NULL);
+
 		while (true)
 		{
 			OnUpdate();
-
+			glfwSwapBuffers(window);
+			glfwPollEvents();
 
 		}
 	}
